0ae3899450a5130f3525162e54327cfe6cf5e95e
ricardowoj <ricardowoj@hotmail.com>
2021-03-29 22:41:12 -0300

    create new project

---
+++ src/main/java/com/educandoweb/curso/CursoNelioAlvesApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoNelioAlvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoNelioAlvesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/educandoweb/curso/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.educandoweb.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User user = new User(1L, "Maria", "maria@uol.com", "1194269000", "123456");
+		return ResponseEntity.ok().body(user);
+	}
+}
